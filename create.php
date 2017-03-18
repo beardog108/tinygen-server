6ed5cb5d6782a9b301e0c02de178a2c691dde086
Kevin Froman <kevinfroman12@gmail.com>
2017-03-18 15:36:22 -0500

    added more upload verification

--- create.php
+++ create.php
@@ -4,27 +4,25 @@ Copyright 2017 Kevin Froman - GNU AFFERO GENERAL PUBLIC LICENSE
 */
 
 function createError($reason, $createType){
+  // Show an error if there was one encountered while creating
   $_SESSION['createError'] = $reason;
   header('location: create.php?type=' . $createType);
   die(0);
 }
 
-function checkIfPost($CSRF){
+function checkIfPost($CSRF, $createType){
   // Check if it was a post (creation upload) request, if so validate it
   $retVal = false;
-  if (isset($_POST['createType'])){
-    $createType = $_POST['createType'];
-    if ($createType != 'plugin' || $createType != 'theme'){
-      header('location: dashboard.php');
-      die(0);
+  if (isset($_FILES['package'])){
+    $retVal = true;
+    if ($_FILES['package']['size'] > 10485760){
+        createError('Your archive is too large', $createType);
     }
-  if ($CSRF != $_POST['csrf']){
-    createError('Invalid request token', $createType);
+    if (pathinfo(basename($_FILES['package']['name']), PATHINFO_EXTENSION) != '.zip'){
+        createError('Your archive is not in the zip format', $createType);
+    }
   }
-  if (isset($_POST['package'])){
-    // Proccess it
-  }
-  }
+  return $retVal;
 }
 
 session_start();
@@ -38,26 +36,37 @@ $userInfo = new userInfo();
 $user = $_SESSION['user'];
 $createType = '';
 
-if (checkIfPost($CSRF)){
-  //createItem()
+$createError = false;
+if (isset($_SESSION['createError'])){
+  if ($_SESSION['createError'] != ''){
+    $createError = true;
+  }
 }
 
-if (isset($_GET['type'])){
-  if ($_GET['type'] == 'plugin'){
-    $createType = 'plugin';
-  }
-  elseif ($_GET['type'] == 'theme'){
-    $createType = 'theme';
+  if (isset($_GET['type'])){
+    if ($_GET['type'] == 'plugin'){
+      $createType = 'plugin';
+    }
+    elseif ($_GET['type'] == 'theme'){
+      $createType = 'theme';
+    }
+    else{
+      header('location: dashboard.php');
+      die(0);
+    }
   }
   else{
     header('location: dashboard.php');
     die(0);
   }
-}
-else{
-  header('location: dashboard.php');
-  die(0);
-}
+  if (! $createError){
+    if (checkIfPost($CSRF, $createType)){
+        createError('Success!', $createType);
+        //header('location: dashboard.php');
+        //die(0);
+    }
+  }
+
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -88,9 +97,8 @@ else{
       }
       ?>
       <div class='center'>
-        <form method='post' action='create.php' onsubmit='return validateUpload("<?php echo $createType;?>");'>
+        <form method='post' action='create.php?type=<?php echo $createType;?>' onsubmit='return validateUpload("<?php echo $createType;?>");' enctype="multipart/form-data">
           <input name='csrf' value='<?php echo $CSRF;?>' type='hidden'>
-          <input name='createType' value='<?php echo $createType;?>' type='hidden'>
           <label class='formArea'><?php echo ucwords($createType);?> Archive (.zip): <input required id='package' type='file' name='package' accept='.zip'></label>
           <label class='formArea'><input class='mainButton' type='submit' value='Create <?php echo ucwords($createType);?>'></label>
         </form>
